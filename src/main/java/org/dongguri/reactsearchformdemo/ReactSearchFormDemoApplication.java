37846c0d429c3e0c21a7fc6fcdc3987c216e263c
sk1737030 <sk1737030@naver.com>
2020-11-05 18:11:09 +0900

    리엑트 및 스프링부트 연동 jar배포시 frontend 같이 실행되게

---
+++ src/main/java/org/dongguri/reactsearchformdemo/ReactSearchFormDemoApplication.java
@@ -0,0 +1,13 @@
+package org.dongguri.reactsearchformdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSearchFormDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSearchFormDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dongguri/reactsearchformdemo/ReactSearchFormDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dongguri.reactsearchformdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSearchFormDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
