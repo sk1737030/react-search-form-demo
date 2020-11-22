4fc717a4763054c1598a8b4b0e57c92acfbb0106
1234 <sk1737030@naver.com>
2020-11-22 22:28:14 +0900

    TFT API 호출 추가

--- src/main/java/org/dongguri/reactsearchformdemo/service/TftApiService.java
+++ src/main/java/org/dongguri/reactsearchformdemo/service/TftApiService.java
@@ -1,33 +1,49 @@
 package org.dongguri.reactsearchformdemo.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.dongguri.reactsearchformdemo.mapper.TftApiMapper;
+import org.apache.http.client.utils.URIBuilder;
 import org.dongguri.reactsearchformdemo.dto.SummonerDTO;
+import org.dongguri.reactsearchformdemo.mapper.TftApiMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.net.URISyntaxException;
 
 @Service
 public class TftApiService {
 
+    private static final String HTTPS_KR_API_RIOTGAMES_COM = "https://kr.api.riotgames.com/";
+    private static final String LOL_SUMMONERS_BY_NAME_API = "lol/summoner/v4/summoners/by-name/";
+
+    @Value("${API_KEY}")
+    private static String TFT_API_KEY;
+
     @Autowired
+    private RestTemplate restTemplate;
+
+    @Resource
     private TftApiMapper summonerMapper;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    public SummonerDTO getSummonerByName(String name) throws Exception {
+        SummonerDTO summonerDTO = summonerMapper.getSummonerByName(name);
 
-    public String getSummonerByName(String name) throws Exception {
-        SummonerDTO summonerVO = summonerMapper.getSummonerByName(name);
-
-        // Api 호출
-        if (summonerVO == null) {
-            summonerVO = callSummonerApiByName(name);
-
+        // API 호출
+        if (summonerDTO == null) {
+            summonerDTO = callSummonerApiByName(name);
         }
 
-        return null;
+        return summonerDTO;
     }
 
-    private SummonerDTO callSummonerApiByName(String name) throws Exception {
+
+    private SummonerDTO callSummonerApiByName(String name) throws URISyntaxException {
+        URIBuilder uriBuilder = new URIBuilder(HTTPS_KR_API_RIOTGAMES_COM + LOL_SUMMONERS_BY_NAME_API + name);
+        uriBuilder.addParameter("api_key", TFT_API_KEY);
+
+        // TODO:: 비동기처리도 따로 가능함. AsyncRestTemplate 사용해서
+        return restTemplate.getForObject(uriBuilder.build(), SummonerDTO.class);
 
     }
 }
